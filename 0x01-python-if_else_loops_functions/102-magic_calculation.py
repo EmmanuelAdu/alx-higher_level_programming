6f76837629af8912538ef2354f330ce41772baa3
EmmanuelAdu <christorenzo20@gmail.com>
2023-08-11 04:10:41 -0700

    This program matches bytecode from holberton

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''This program match bytcode provided by holberton sch.'''
+
+
+def magic_calculation(a, b, c):
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a * b - c)
