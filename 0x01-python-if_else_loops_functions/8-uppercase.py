0b34421b88347dced87d6fd03f672cf6147e22d3
EmmanuelAdu <christorenzo20@gmail.com>
2023-08-11 18:10:21 -0700

    This function converts a string to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -7,5 +7,5 @@ def uppercase(str):
     for char in str:
         if 'a' <= char <= 'z':
             char = chr(ord(char) - ord('a') + ord('A'))
-        print("{}".format(char), end="")
-    print("")
+        result += char
+    print(result, end="\n")
