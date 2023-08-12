e3eda3bb9cbd16bc19a82008cce4b40da300ec0a
EmmanuelAdu <christorenzo20@gmail.com>
2023-08-11 23:30:50 -0700

    This program prints the number and list of its argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+''' THIS PROGRAM PRINTS THE NO. AND LIST OF ARGUMENT '''
 
+
+if __name__ == "__main__":
+    import sys
+    find_len = len(sys.argv) - 1
+    if find_len == 0:
+        print("0 arguments.")
+    elif find_len == 1:
+        print("{} argument:\n{}: {}".format(find_len, find_len, sys.argv[1]))
+    else:
+        print("{} arguments:".format(find_len))
+        for i in range(1, find_len + 1):
+            print("{}: {}".format(i, sys.argv[i]))
