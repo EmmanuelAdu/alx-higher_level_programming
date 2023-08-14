dfbbf6c94252952671891b73183133a3b9e52cd5
EmmanuelAdu <christorenzo20@gmail.com>
2023-08-14 11:50:25 -0700

    This function returns replaced element in a copied list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,8 +5,8 @@
 def new_in_list(my_list, idx, element):
     cpy_list = list(my_list)
 
-    if idx < 0 or idx > len(my_list):
-        return (cpy_list)
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
     else:
         cpy_list[idx] = element
         return (cpy_list)
