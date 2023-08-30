efa48630421c1feb3e626ddb15ed67b10c1c55f7
EmmanuelAdu <christorenzo20@gmail.com>
2023-08-29 20:37:42 -0700

    This function defines a class square with private instance size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Define A Class Square'''
+
+
+class Square:
+    '''Initializing a private object size
+
+    Args:
+         size(int): the size of the square
+    '''
+    def __init__(self, size):
+        self.__size = size
