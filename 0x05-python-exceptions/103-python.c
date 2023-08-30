be2129e44ab58f68db93932f590a4413c298c6b0
EmmanuelAdu <christorenzo20@gmail.com>
2023-08-30 16:46:38 -0700

    This function print some basic info about Python lists, bytes, float

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,42 @@
+#include <Python.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+/**
+ * print_python_list - this function prints info about python list
+ * @p: pointer to the python list
+ */
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t alloc;
+	Py_ssize_t i, size;
+	const char *type;
+	PyListObject *list = (PyListObject *)p;
+	PyListObject *var = (PyListObject *)p;
+
+	size = var->ob_size;
+	alloc = list->allocated;
+
+	fflush(stdout);
+
+	printf("[*] Python list info\n");
+	if (strcmp(p->ob_type->tp_name, "list") != 0)
+	{
+		printf(" [ERROR] Invalid List Object\n");
+		return;
+	}
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		type = list->ob_item[i]->ob_type->tp_name;
+		printf("Element %ld: %s\n", i, type);
+		if (strcmp(type, "bytes") == 0)
+			print_python_bytes(list->ob_item[i]);
+		else if (strcmp(type, "float") == 0)
+			print_python_float(list->ob_item[i]);
+	}
+}
