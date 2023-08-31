a917e8ea872a7102dc9d50e48faa88766b38c2bd
EmmanuelAdu <christorenzo20@gmail.com>
2023-08-30 18:43:50 -0700

    This program print some basic info about Python lists, Python bytes an Python float objects

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -40,3 +40,46 @@ void print_python_list(PyObject *p)
 			print_python_float(list->ob_item[i]);
 	}
 }
+
+/**
+ * print_python_bytes - this prints bytes info
+ * @p: pointer to the bytes location
+ */
+void print_python_bytes(PyObject *p)
+{
+	PyBytesObject *bytes = (PyBytesObject *)p;
+	PyVarObject *var = (PyVarObject *)p;
+
+	Py_ssize_t size = var->ob_size;
+	char *str = PyBytes_AsString(p);
+
+	fflush(stdout);
+	printf("[.] bytes object info\n");
+	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
+	{
+		printf(" [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", str);
+	printf("  address of raw bytes: %p\n", (void *)str);
+}
+
+/**
+ * print_python_float - prints float
+ * @p: pointer to float
+ */
+void print_python_float(PyObject *p)
+{
+	double value = PyFloat_AsDouble(p);
+
+	fflush(stdout);
+
+	printf("[.] float object info\n");
+	if (strcmp(p->ob_type->tp_name, "float") != 0)
+	{
+		printf(" [ERROR] Invalid Float Object\n");
+		return;
+	}
+	printf("  value: %lf\n", value);
+}
