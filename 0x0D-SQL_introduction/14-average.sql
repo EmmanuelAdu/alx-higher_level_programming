3608db78062da994e0586ceff3dddc2bc84583ae
EmmanuelAdu <christorenzo20@gmail.com>
2023-10-17 22:52:52 +0100

    Solution to task14

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- the score average of all records in the table second_table of the 
+--   database hbtn_0c_0 in your MySQL server.
+--   the result column name should be average
+SELECT AVG(score) AS average FROM second_table;
