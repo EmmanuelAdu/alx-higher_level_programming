a705b0f2254fbdf817b0ef3ee17a6ef08bea9dec
EmmanuelAdu <christorenzo20@gmail.com>
2023-10-19 01:28:31 +0100

    Solution to task10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+
+SELECT ts.title, tsg.genre_id
+FROM tv_shows AS ts
+INNER JOIN tv_show_genres AS tsg
+ON ts.id=tsg.show_id
+ORDER BY ts.title, tsg.genre_id;
